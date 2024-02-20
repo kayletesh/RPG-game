b09fd1d2c36e8f8a45c89f4bcae6034d59472285
kayletesh <kayletesh@gmail.com>
2024-02-19 21:53:29 -0500

    initial commit add boilerplate add buttons and initial info

---
+++ script.js
@@ -0,0 +1,16 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = [
+  "stick",
+  //   "dagger",
+  //   "shortSword",
+  //   "spear",
+  //   "greatSword",
+  //   "fireBlast",
+];
+
+let buttonOne = document.querySelector("#buttonOne");
