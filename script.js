3e86cbb4b77cbbcde099b422ed4722e5b300fead
kayletesh <kayletesh@gmail.com>
2024-02-24 20:44:07 -0500

    set all v0.1 examine Text clean up code determine directional path from village

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ const locationData = [
   {
     /*onLoad*/ name: "start",
     btnContents: [
-      'Enter "Axes and Alms"',
+      'Enter "Axes and Amenities"',
       'Enter "The Last Dram"',
       "Set Forth",
       "Examine The Village",
@@ -142,38 +142,43 @@ const locationData = [
       },
     ],
     displayText:
-      "<span><strong>Welcome</strong> young adventurer, to the village of Sonir! Strange creatures have appeared beyond the village gates. The king calls for aid! <br><strong>The Bone King</strong> has risen once more! You must train hard, and venture far from home to help save the kingdom from this terrible threat.<br><br> What do you wish to do?</span>",
+      "<span><strong>Welcome</strong> young adventurer, to the village of Sonir. Strange creatures have appeared beyond the village gates. <strong>The King of Covehn has called for aid!</strong> All those young, and capable of travel, are to make for the kingdom at once! <br><br><strong>The Bone King</strong> has risen once more! <br> You must train hard, and venture far from home to help save the kingdom from this terrible threat.<br><br> What do you wish to do?</span>",
     examineOptions: {
       btnContents: [
-        'Examine "Axes and Alms"',
-        'Examine "The Last Dram"',
-        "Examine Village Gates",
+        "Examine the Shop",
+        "Examine the Tavern",
+        "Examine the Village Gates",
         "Return",
       ],
       btnFunctions: [
         function () {
-          examine("Examining the storeFront");
+          examine(
+            '<span>You examine the little shop.<br><br> It\'s really nothing more than a large, round, wooden door build into the side of a hill. An equally round, lovingly painted, red sign is displayed with pride above the little door:<br><br> <strong>"AXES AND AMENITIES:<br>ONE STOP SHOP FOR ALL YOUR<br> ADVENTURING SUPPLIES!"</strong> <br><br> What do you wish to do?</span>'
+          );
         },
         function () {
-          examine("Examining the Bar");
+          examine(
+            "<span>You examine the tavern.<br><br> The village's tavern sits to your right. It's a simple structure of carved stone and wood, but your eyes quickly fall upon a well kept sign standing by the door. <br><br><strong>\"THE LAST DRAM <br>BEST ALE SOUTH OF THE VEREL FOREST!<br>JUST ONE SIP, AND ALL YOUR <br>SORROWS WILL FADE AWAY!</strong>\"<br><br> What do you wish to do?</span>"
+          );
         },
         function () {
-          examine("examining village gates");
+          examine(
+            "<span>You examine the Village Gates.<br><br> The gates are old, as tall as a mountain troll, and made of thick, sturdy dark oak logs.<br><br> Given the residents of this village, it's no wonder their defenses are so strong. Why, it would take at least <strong>two</strong> mountain trolls to break through that gate!<br><br> What do you wish to do?</span"
+          );
         },
         function () {
           goBack(locationData[1]);
         },
       ],
-      displayText: "<span>What would you like to examine?</span>",
+      displayText:
+        "<span>The village of Sonir is very small, with fewer than 200 residents within it's walls. From the village square, you can determing that there is only one shop, and one tavern here. Despite this, the little village is known for two things:<br><br> Due to it's quaint location far from any warfronts, many aging warriors find thier way to Sonir to await thier final rest.<br><br> What is the other thing the village is known for, you ask? Why its their <strong>World Famous EmberBerry Ale</strong> of course!!</span>",
     },
-    // examineText:
-    //   '<span>Upon examining the village you notice three things.<br> To your left, the village\'s only trading post; <strong>"Axes and Alms"</strong>.<br> To your right, a small tavern; <strong>"The Last Dram"</strong>, A sign near the door reads: <br> "Looking for work? Willing to brave the world outside the walls? Inquire inside." <br>Ahead of you are the village gates; the only way in or out of Sonir.</span>',
   },
   // base set[town1, store1, bar1, path1]
   {
     /*Sonir */ name: "Sonir",
     btnContents: [
-      'Enter "Axes and Alms"',
+      'Enter "Axes and Amenities"',
       'Enter "The Last Dram"',
       "Set Forth",
       "Examine the Village",
@@ -187,7 +192,7 @@ const locationData = [
       },
     ],
     displayText:
-      "<span>You have returned to the village square.<br><br> What will you do next?</span>",
+      "<span>You have returned to the village square. <br><br>The village crier announces the King's call to arms once more. <br><br> What will you do next?</span>",
     examineOptions: {
       btnContents: [
         'Examine "Axes and Alms"',
@@ -197,23 +202,30 @@ const locationData = [
       ],
       btnFunctions: [
         function () {
-          examine("Examining the storeFront");
+          examine(
+            '<span>You examine the storefront once more.<br><br> Upon closer examination you recognize the wood of the door as Blue Mahoe. It\'s a rare wood, taken from the beautiful Blue Rosewood trees from the southern shores.<br><br> You see an addition to the sign, a little rectangular pice of scrap wood on a chain which reads:<br><br><strong>"FRESH POTIONS BREWED DAILY!"</strong><br><br> What do you wish to do? </span>'
+          );
         },
         function () {
-          examine("Examining the Bar");
+          examine(
+            "<span>You examine the tavern's entrance once more.<br><br> There have been no significant changes since you last surveyed the tavern.<br><br> What do you wish to do?</span>"
+          );
         },
         function () {
-          examine("examining village gates");
+          examine(
+            "<span>You examine the Village Gates once more. <br><br> Two fae males stand by the gate, waiting patiently for the shift in the gaurd. Though their faces remain pristine without a wrinkle, or crows foot in sight, any who have learned to read the fae's eyes would know these two to be ancient in mortal terms.<br> A sign that even fae warriors find their final days peaceful here in Sonir. <br><br> What do you wish to do?</span>"
+          );
         },
         function () {
           goBack(locationData[1]);
         },
       ],
-      displayText: "<span>What would you like to examine?</span>",
+      displayText:
+        "<span>Not much has changed since you last surveyed the village. a few people glance your way as they go about their days.<br><br> What do you wish to do?</span>",
     },
   },
   {
-    /*axes and alms*/ name: "Axes and Alms",
+    /*axes and amenities*/ name: "Axes and Amenities",
     btnContents: [
       "Buy Items",
       "Sell Items",
@@ -229,7 +241,7 @@ const locationData = [
       },
     ],
     displayText:
-      '<span>You duck into the shop to find the storefront is not much larger than a halfling\'s hole. There are two small doors leading off to different rooms on either side; both with large signs stating <strong>"NO ENTRY"</strong> <br>An elderly dwarven male watches you from the counter. He speaks in a low, gruff voice: <br>"Hello, young one... Anything catch\'n yer eye?"<br><br> What will you do next?</span>',
+      '<span>You duck inside to find the interior of the shop is not much larger than a halfling\'s home. There are two small doors leading off to different rooms on either side; both with large signs stating:<br><br> <strong>"NO ENTRY"</strong> <br><br>An elderly dwarven male watches you from the counter. He speaks in a low, but gentle voice: <br>"Hello, young one... Anything catch\'n yer eye?"<br><br> What will you do next?</span>',
     examineOptions: {
       btnContents: [
         "Examine the Room",
@@ -239,19 +251,26 @@ const locationData = [
       ],
       btnFunctions: [
         function () {
-          examine("examining the room");
+          examine(
+            "You examine the interior of the shop more closely. there are several displays along the walls, and in the center of the room.<br><br> Some are laiden with books and Tomes. Some are covered in plants of all kinds(Some of which seem to move when you're not watching them.) Some are filled with delicious looking fruits and root vegetables. There's a rack behind the counter where two ducks, a very plump pheasant, and some creature you can't quite identiy hang, waiting for purchase.<br><br> There are two racks near the counter. One is covered in axes, swords, daggers, and you even spot a worn spear! The other displays three full sets of leather armour, one seemingly crafted to fit a halfling, one a bit larger, fit for a dwarf, and the other tall and lean, with an elf's signet embossed at the cuffs.<br><br> What do you wish to do?"
+          );
         },
         function () {
-          examine("examining the dwarf");
+          examine(
+            "You examine the dwarf. The first thing you notice is that he is old. <em><strong>Really</strong></em> old. <br><br>His once red hair is now mostly grey, but a few strands of fire red still fight through tangled mane. His beard grows nearly to the ground, showing this dwarf has never fallen in battle, and tied in multiple grey-and-red braids.<br><br>There are three long, jagged scars running from his left temple, down to his chin, cutting across his left eye, nose, and lip.<br>The eye in question, has been replaced by a stunning golden device that seems to track your movements as if it were truly watching you.<br><br>The dwarf wears a simple green tunic, revealing many similarly grisly scars along his arms and legs. It is clear he has seen many battles in his earlier days.<br><br> What do you wish to do?"
+          );
         },
         function () {
-          examine("speaking to the dwarf");
+          examine(
+            "\"Hahah, It's about time I got a <em><strong>curious</strong></em> adventurer through my doors! Well, come little one. What's on yer mind?\" He asks with a friendly laugh that seems to boom in the small space. <br><br> What do you wish to ask?"
+          );
         },
         function () {
           goBack(locationData[1]);
         },
       ],
-      displayText: "<span>What would you like to examine?</span>",
+      displayText:
+        "<span>The dwarf raises an eyebrow as you examine his establishment. <br><br> What do you wish to do?</span>",
     },
   },
   {
@@ -272,7 +291,7 @@ const locationData = [
       },
     ],
     displayText:
-      "<span>You enter the small tavern. There are a few other travelers at the tables. A human male with what looks to be a long healed burn scar across the right side of his face nods in a silent greeting.<br><br> What will you do?</span>",
+      "<span>You enter the small tavern. There are a few other travelers at the tables. A halfling male with what looks to be a long healed burn scar across the right side of his face nods your way in a silent greeting.<br><br> What will you do?</span>",
     examineOptions: {
       btnContents: [
         "Examine your surroundings",
@@ -282,23 +301,30 @@ const locationData = [
       ],
       btnFunctions: [
         function () {
-          examine("examining surroundings");
+          examine(
+            "You examine your surroundings more closely.<br><br> Behind the bar are several massive kegs, each labeled with a different type of ale, whiskey, or rum.<br> Above the bar, several trophy heads look back down at you with taxidermied eyes. <br><br>Of note, you see: a wolf, a barrel rat, it's head nearly the size of your own, and a plains cat, only slightly larger than the rat.<br><br> What do you wish to do?"
+          );
         },
         function () {
-          examine("examining the barkeep");
+          examine(
+            'You examine the barkeep more closely. Upon further examination, you realize the little male is standing atop not one, but <em><strong>Three</strong></em> crates! He notices your wide eye\'d expression and growls.<br><br><strong>"My eyes are up here, pal!!"</strong><br> He huffs, before pointing to the menu board. <br>"Either buy sum\'tin or get out!"<br><br>What do you wish to do?'
+          );
         },
         function () {
-          examine("speaking to barkeep");
+          examine(
+            'The barkeep lets out another annoyed growl, then lets out an exasperated sigh.<br><br> "OH FOR THE LUV OF... CAN\'T YOU SEE I\'M BUSY HERE??"<br> The little halfling lets out a frustrated growl while filling another dram and sending it down the bar to another patron. With the order filled, he lets out another long, suffering sigh, then turns to face you.<br><br> "Fine... fine..." He grumbles.<br> "What? What do you want to know?"'
+          );
         },
         function () {
           goBack(locationData[3]);
         },
       ],
-      displayText: "<span>What would you like to examine?</span>",
+      displayText:
+        "<span>You examine the tavern.<br><br> The space is much larger than you expected.<br> Several patrons sit at the round tables throughout, and near the back, a hooded figure seems to be asleep in one of the booths adorning the walls.<br><br> What do you wish to do?</span>",
     },
   },
   {
-    /*pathOne */ name: "pathOne",
+    /*pathOne */ name: "RoadOne",
     btnContents: [
       "Explore the Plains",
       "Venture Further From Sonir",
@@ -318,25 +344,32 @@ const locationData = [
     examineOptions: {
       btnContents: [
         "Examine the Plains",
-        "Examine the Path Ahead",
-        "Examine emberBerry patch",
+        "Examine the Road Ahead",
+        "Examine the EmberBerry patch",
         "return",
       ],
       btnFunctions: [
         function () {
-          examine("examining the plains");
+          examine(
+            "<span>You examine the Plains. The sea of green is broken periodically by copses of trees. <br><br>To the east you can see an old farmhouse.<br> To the west, the berry patch is just visible in the distance.<br><br>Behind you lies the village, and in the distance, you can just make out the scarecrow that marks the crossroads to the south.<br><br> What do you wish to do?</span>"
+          );
         },
         function () {
-          examine("examining the path ahead");
+          examine(
+            "<span>You examine the road ahead.<br><br>You can see what looks like a scarecrow far to the south, and know it to be the crossroads that leads away from the village. <br>The road is well kept, and even now you can see the village guard on their patrol. So long as you stick to the road, you will be safe.<br><br> What do you wish to do?</span>"
+          );
         },
         function () {
-          examine("examining the berry Patch");
+          examine(
+            "You examine the berry patch.<br><br> There is not much that you can discern from this distance. The berry patch lies far to the west.<br><br> What do you wish to do?"
+          );
         },
         function () {
           goBack(locationData[4]);
         },
       ],
-      displayText: "<span>What would you like to examine?</span>",
+      displayText:
+        "<span>The plains seem to stretch on for miles ahead of you.<br><br> The lush field of green is tall enough to reach a halfling's chin. Every now and then, you can see the grass rustle, and you're fairly certain that was a tail breaching the grass up ahead.<br><br> What do you wish to do?</span>",
     },
   },
   /*all examine function options*/
